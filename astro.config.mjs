890ee734eccd0c49b797e7811693153e2db299a2
Luke Boyett <lboyett@gmail.com>
2026-01-27 16:37:55 -0500

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tradebuilt-web.github.io",
-  base: "/tradebuilt-landing-page/",
+  site: "https://tradebuiltwebdesigns.com/",
   vite: {
     plugins: [tailwindcss()],
   },
